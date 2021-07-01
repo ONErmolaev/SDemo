ac904a133f79c6cc4f4db8816cf599da3f326fd2
Oxana Ermolaev <ONErmolaev@gmail.com>
2021-07-01 10:26:05 -0400

    Removed unused fields and arguments

--- src/test/java/runner/CucumberBase.java
+++ src/test/java/runner/CucumberBase.java
@@ -2,24 +2,22 @@ package runner;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
 import tests.BaseTest;
 
 
 public class CucumberBase extends BaseTest {
 
-    private static long startMillis;
     private static BaseTest baseTest = null;
 
     @Before
-    public static void before(Scenario scenario) {
+    public static void before() {
         baseTest = new CucumberBase();
         baseTest.startup();
     }
 
     @After
-    public static void after(Scenario scenario) {
+    public static void after() {
         baseTest.finishUp();
     }
 
